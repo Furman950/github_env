0c519f2ee7a701658522bccefca64e1fa31194cd
Fermin Sandoval <ferminsandoval950@hotmail.com>
2021-12-21 13:39:46 -0700

    Yet again another change

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Github Actions')
+    print_hi('Fermin Sandoval')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
