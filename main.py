fd8f2564ed1201fe9708a0a9c8d349810d33291c
Fermin Sandoval <ferminsandoval950@hotmail.com>
2021-12-23 17:11:52 -0700

    new change

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Fermin Sandoval')
+    print_hi('Fermin Sandoval qa change')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
